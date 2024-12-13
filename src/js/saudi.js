8c3daa73b0d5b91b1c2fb602dc1e45cc3c991db1
yahia-raheem <eng.yahiaraheem.93@outlook.com>
2024-12-13 19:31:02 +0200

    feat(saudi): central content 5 done

--- src/js/saudi.js
+++ src/js/saudi.js
@@ -1,5 +1,6 @@
 import "./components/animate.js";
 // import "./components/popup.js";
 import "./components/loader.js";
+import "./components/tabs.js";
 
 document.addEventListener("DOMContentLoaded", () => {});
